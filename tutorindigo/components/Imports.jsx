60704d85e960af4973c6f7cfafe8e133fa37cd7d
Mahendra <MahendraC@TitanEd.com>
2026-03-09 14:07:28 +0530

    Updated imports

--- tutorindigo/components/Imports.jsx
+++ tutorindigo/components/Imports.jsx
@@ -5,3 +5,14 @@ import { getConfig } from '@edx/frontend-platform';
 import { Icon } from '@openedx/paragon';
 import { Nightlight, WbSunny } from '@openedx/paragon/icons';
 import { useIntl } from '@edx/frontend-platform/i18n';
+import classNames from 'classnames';
+import {
+    LocationOn,
+    Mail,
+    Phone,
+    BsFacebook,
+    BsLinkedin,
+    BsInstagram,
+    BsYoutube,
+    BsTwitter,
+    ArrowRight, } from '@openedx/paragon/icons';
--- tutorindigo/components/IndigoFooter.jsx
+++ tutorindigo/components/IndigoFooter.jsx
@@ -1,18 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import {
-//   LocationOn,
-//   Mail,
-//   Phone,
-//   BsFacebook,
-//   BsLinkedin,
-//   BsInstagram,
-//   BsYoutube,
-//   BsTwitter,
-//   ArrowRight, } from '@openedx/paragon/icons';
-// import { useIntl } from '@edx/frontend-platform/i18n';
-// import classNames from 'classnames';
-// import { Icon } from '@openedx/paragon';
-// import { getConfig } from '@edx/frontend-platform';
 const IndigoFooter = () => {
   const intl = useIntl();
   const config = getConfig();
