ef22810b39e0d47156a2492f83f37ae8cd47f634
Sonu-TitanEd <SonuK@TitanEd.com>
2026-07-21 11:25:44 +0530

    fix header brackdown build fail

--- tutorindigo/components/Imports.jsx
+++ tutorindigo/components/Imports.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import Cookies from 'universal-cookie';
-import Responsive from 'react-responsive';
 
 import { getConfig } from '@edx/frontend-platform';
 import { AppContext } from '@edx/frontend-platform/react';
@@ -8,7 +7,6 @@ import { Icon } from '@openedx/paragon';
 import { Nightlight, WbSunny } from '@openedx/paragon/icons';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import classNames from 'classnames';
-import MobileHeader from '@edx/frontend-component-header/dist/mobile-header/MobileHeader';
 import {
     AccountCircle,
     LocationOn,
